06c1dd59140394e36f5a297e96d5210db0192136
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-01-12 14:21:07 +0000

    #7 Update comments and parameters

--- src/simple/definitions.py
+++ src/simple/definitions.py
@@ -9,11 +9,16 @@ for use by other modules.
 import os
 
 # Set the directory paths relative to this file
-SRC_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Note - only parent dir is reliable for use in user vs dev installs
+# repo/src/package (dev) or site-packages/package (user)
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-REPO_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Do not use refs to SRC_DIR or REPO_DIR
 
 # Set the package name
 # TODO check r.e. take name from setup.py to minimise duplication
 PACKAGE = "simple"
+
+if __name__ == "__main__":
+    # Execute when the module is not initialized from an import statement.
+    print(f"definitions file: {os.path.abspath(__file__)}")
+    print(f"package dir: {PACKAGE_DIR}")
