93ab8ec0f76e955a09d8839b8196812bd63f706b
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-01-18 17:54:51 +0000

    #7 Adds tests for resources dir

---
+++ tests/d_user_interface/resources/test_resources.py
@@ -0,0 +1,26 @@
+"""Tests for the resources namespace subpackage."""
+
+from importlib.resources import files
+
+from simple.definitions import RESOURCES
+
+DEBUG = True
+
+
+def test_data_resources():
+    """Test that package resources canb e accessed via namespace."""
+    package_resources = files(RESOURCES)
+    assert package_resources.is_dir()
+    content = package_resources.iterdir()
+    expected_filenames = ["data1.txt", "data2.csv"]
+    for file in content:
+        if DEBUG:
+            print(file)
+        assert file.name in expected_filenames
+        assert file.is_file()
+
+
+def test_data_resources_file_content():
+    """Test that data file content can be read."""
+    data_text = files(RESOURCES).joinpath("data1.txt").read_text()
+    assert data_text == "hello world"
