15101811a6c86d34d750569cf00c9e04b2882d38
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-01-25 16:42:24 +0000

    #7 Add new command line tools

--- src/simple/cli.py
+++ src/simple/cli.py
@@ -16,6 +16,7 @@ import pkg_resources
 
 from simple.analysis.analysis import calculate
 from simple.definitions import PACKAGE
+from simple.netcdf.data import main
 
 DEBUG = True
 """bool: Debugging level, module level constant (Default: True)."""
@@ -25,7 +26,7 @@ pkg_version = pkg_resources.get_distribution(PACKAGE).version
 
 
 def cli_entry_point(argv=None):
-    """CLI tool, pass command line arguments to the analysis calculate function."""
+    """Command line tool for analysis calculate function."""
     parser = argparse.ArgumentParser(
         prog="CLI-SIMPLE",
         description="A simple app to conduct analysis on two integers",
@@ -52,5 +53,35 @@ def cli_entry_point(argv=None):
     # run analysis calculation using the user provided input args
     result = calculate(parsed_args.x, parsed_args.y)
     print(result)  # print to stdout, don't return value
+    # Note cli tools may be expected to return none or 0 for testing
+    # When developing tests for cli tools, check the use of returncode
+
+
+def cli_data(argv=None):
+    """Command line tool for data creation, with options."""
+    parser = argparse.ArgumentParser(
+        prog="CREATE-DATA",
+        description="Create data files in datadir.",
+        epilog="  ---  ",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    # Arguments in argparse can be positional or optional
+    # Set the argument type and limit choices from a list
+    # Note type is set to int, to force conversion
+
+    parser.add_argument(
+        "--verbose", action="store_true", help="Print progress and data info to stdout"
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",  # Prints version information and exits when invoked
+        help="Display the version of the cli tool",
+        version=f"{parser.prog} {pkg_version}",
+    )
+
+    parsed_args = parser.parse_args(argv)
+    # run using the user provided input args
+    main(debug=parsed_args.verbose)
     # Note cli tools may be expected to return none or 0 for testing
     # When developing tests for cli tools, check the use of returncode
