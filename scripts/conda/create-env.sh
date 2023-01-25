8aca36c98183dec97548f7dff8a24dc8194726b2
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-01-25 17:22:27 +0000

    #7 Add conda init

--- scripts/conda/create-env.sh
+++ scripts/conda/create-env.sh
@@ -10,6 +10,10 @@ CODE_DIR="$(dirname $(dirname "${PWD}"))"
 # Set name of environment file holding dependencies list
 CONA_ENV_FILE='environment.yml'
 
+# ensure conda commands are accessible (required to run)
+conda init bash > /dev/null 2>&1
+eval "$(conda shell.bash hook)"
+
 echo "Creating conda environment from: ${CONA_ENV_FILE}"
 conda env create -f "${CODE_DIR}/${CONA_ENV_FILE}"
 
