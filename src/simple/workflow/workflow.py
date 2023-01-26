620eb096b2cf886293d9f1c9970a52778f8c37e8
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-01-26 09:37:02 +0000

    #7 Add workflow WIP

---
+++ src/simple/workflow/workflow.py
@@ -0,0 +1,49 @@
+"""Workflow for the simple analysis system."""
+
+# KISS !
+
+# note distinction python called vs command line at terminal
+
+# system dependencies
+# installed into appropriate location or environment
+
+# step 0 (optional)
+# -----------------
+
+# user confirms or edits the system config
+# this determines data and scratch (temp) directories
+# log settings to file
+# link to notes / discussion / issue
+# if config setting should be committed as part of the repo?, separate repo?
+
+# step 1
+# ------
+
+# create basic netcdf, CF compliant, with simple data content
+# write to file (as specified by config)
+
+# step 2
+# ------
+
+# view netcdf content
+# describe content in high detailed, labeled, very descriptive
+
+
+# step 3
+# ------
+
+# test netcdf compliance - both tools
+
+
+# step x
+# ------
+
+# run cli tool X  to take user input and return analysis output to the terminal
+
+# step y
+# ------
+
+# ? AMEND
+# amend cli tool, or another to  take user input, conduct analysis and write to netcdf
+# write netcdf to outputs dir
+# log process, including record user inputs
