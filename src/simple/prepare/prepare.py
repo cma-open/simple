1a8f7a95aa2302f46c3fdbe6d2a90350829581c0
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-02-01 17:36:18 +0000

    #25 Avoid override built-in

--- src/simple/prepare/prepare.py
+++ src/simple/prepare/prepare.py
@@ -1,13 +1,13 @@
 """Prepare module provides data conversion and preparation code."""
 
 
-def add_small_constant(input):
+def add_small_constant(input_value):
     """Add small constant decimal value to the input value."""
-    result = input + 0.000005
+    result = input_value + 0.000005
     return result
 
 
-def convert_to_string(input):
+def convert_to_string(input_value):
     """Convert input values to string."""
-    string = str(input)
+    string = str(input_value)
     return string
