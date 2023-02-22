266149d351a2c78533449d91687c81a8223123ea
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-02-22 18:29:08 +0000

    #25 Update comments

--- src/simple/io/io.py
+++ src/simple/io/io.py
@@ -19,4 +19,14 @@ def main():
     """Remove a known set of files from the outputs directory."""
     if DEBUG:
         print(f"Removing any existing data files from: {OUTPUTS}")
-    clean_directory(dir=OUTPUTS, files=FILES)
+    clean_directory(dir_path=OUTPUTS, files=FILES)
+
+
+# ==================================================================================
+# Test type and location (training use)
+# ----------------------------------------------------------------------------------
+# a_unit  test_io.py
+# b_integration  test_io.py
+# c_end_to_end  n/a
+# d_user_interface  n/a
+# ==================================================================================
