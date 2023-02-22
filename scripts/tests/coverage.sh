97c8ab752ef465c76ec90d39fa8c80c1e3ef56a0
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-02-22 11:29:05 +0000

    #25 Update comments and stdout

--- scripts/tests/coverage.sh
+++ scripts/tests/coverage.sh
@@ -7,20 +7,18 @@
 # Source code, variables from tests/common.sh
 # Makes CODE_DIR etc available. Also sources common/common.sh
 source common.sh
-
 echo "Current working directory: ${PWD}"
+echo "Move to test dir and run pytest with coverage: ${TESTS_DIR}"
+echo "Run coverage against package name: ${PACKAGE}"
 
-# ensure conda commands are accessible
-conda init bash > /dev/null 2>&1
-eval "$(conda shell.bash hook)"
+cd ${TESTS_DIR}
 
-# activate conda env
-conda activate "${ENV_NAME}"
-
+# TODO check install vs dev test coverage reports
 # run tests with coverage output
 pytest --cov-config="${CODE_DIR}/.coveragerc"  \
        --cov-report term \
-       --cov="${CODE_DIR}" "${TESTS_DIR}"
+       --cov="${PACKAGE}"
+       # note uses PACKAGE to cover both install types
 
 #######################################################################################
 # Code review and system context notes
