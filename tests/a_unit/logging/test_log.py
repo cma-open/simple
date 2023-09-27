a128726fee1096219fa281b1bcf9a9a394a92520
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2023-09-27 11:21:57 +0100

    #25 Updates tests

--- tests/a_unit/logging/test_log.py
+++ tests/a_unit/logging/test_log.py
@@ -75,42 +75,49 @@ def test_create_config_logger(tmp_path):
 
 
 # --------------------------------------------------------------------------------------
-# Test for log outputs from demo functions
+# Test for log outputs from demo_temp functions
 # --------------------------------------------------------------------------------------
 def test_demo_system_logger(tmp_path, caplog, capsys):
     """Test the demo_system_logger function."""
     log_file = tmp_path / "test.log"
     demo_system_console_log(log_file)
-    # Test log message is as expected
-    assert "System demo started" in caplog.text
-    assert "System demo" in caplog.text
+    # Test log message is as expected (captured log, as written to file)
+    assert "System demo_temp started" in caplog.text
+    assert "System demo_temp" in caplog.text
     assert "System warning" in caplog.text
-    assert "System demo finished" in caplog.text
+    assert "System demo_temp finished" in caplog.text
     # Test expected number of log records
     assert len(caplog.records) == 4
     # Test log name
     assert caplog.records[0].name == "SystemLog"
-    # Test that only some output to console
+    # Capture the output to console
     captured = capsys.readouterr()
+    # Set expected output
     expected = (
-        "SystemLog :: INFO     "
-        ":: System demo                        \n"
+        "SystemLog :: INFO     :: System demo_temp                   \n"
         "SystemLog :: WARNING  :: System warning                     \n"
     )
+    # Test that only some output to console
     assert captured.err == expected
 
 
-def test_demo_config_logger(tmp_path, caplog):
+def test_demo_config_logger(tmp_path, caplog, capsys):
     """Test the demo_config_logger function."""
     test_log_file = tmp_path / "test.log"
-    # Run the demo logger
+    # Run the demo_temp logger
     demo_config_file_log(test_log_file)
     # Test file was created
     assert test_log_file.is_file()
 
     # Test log message is as expected
-    assert "Config demo started" in caplog.text
-    assert "Config demo" in caplog.text
-    assert "Config demo finished" in caplog.text
+    assert "Config demo_temp started" in caplog.text
+    assert "Config demo_temp" in caplog.text
+    assert "Config demo_temp finished" in caplog.text
     assert len(caplog.records) == 3
+    # Test log name is as expected
     assert caplog.records[0].name == "ConfigLog"
+    # Capture the output to console
+    captured = capsys.readouterr()
+    # Test that there is no ouput to the console
+    # ConfigLog is expected to be to file only
+    assert captured.err == ""
