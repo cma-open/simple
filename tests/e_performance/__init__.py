641af2332e945bc3fa04fdc3c4f7c4b906c097c7
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2024-12-24 16:41:54 +0000

    #38 Add further tests

---
+++ tests/e_performance/__init__.py
@@ -0,0 +1,17 @@
+"""Performance tests.
+
+Tests may cover:
+- speed
+- scalability
+- bottlenecks
+- resource use
+
+Categories include:
+- load tests
+- stress tests
+- spike tests
+- endurance tests
+- scalability tests
+
+
+"""
